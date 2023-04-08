c0440814ca807b0973cad4fc7e5ce947de7f1d6d
avitenzer <tenzer@clara.co.uk>
2023-04-08 21:50:18 +0300

    Integrate with openai

--- codemaker/codeapp/views.py
+++ codemaker/codeapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib import messages
+import os
+import openai
 def home(request):
+    print(os.getenv('API_KEY'))
     lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'css-extras', 'dart', 'django', 'fsharp', 'go', 'go-module', 'gradle', 'graphql', 'groovy', 'java', 'javadoc', 'javadoclike', 'javascript', 'json', 'json5', 'jsonp', 'jsx', 'markup', 'markup-templating', 'nginx', 'objectivec', 'perl', 'php', 'phpdoc', 'plsql', 'powershell', 'python', 'r', 'ruby', 'rust', 'sas', 'sass', 'scala', 'scss', 'sql', 'tcl', 'tsx', 'typescript', 'typoscript', 'xml-doc', 'yaml']
     if request.method == 'POST':
         code = request.POST.get('code')
@@ -10,6 +13,22 @@ def home(request):
             messages.success(request, 'Please select a programming language')
             return render(request, 'home.html', {'code': code, 'lang': lang, 'lang_list': lang_list})
 
-        return render(request, 'home.html', {'code': code, 'lang': lang, 'lang_list': lang_list})
+        #OPENAI
+        openai.api_key = os.getenv('OPENAI_API_KEY')
+        openai.Model.list()
+        try:
+            response = openai.Completion.create(
+                engine = "text-davinci-003",
+                prompt = f"Respond only with code. Fix this {lang} code: {code}",
+                temperature = 0,
+                max_tokens = 1000,
+                top_p = 1.0,
+                frequency_penalty = 0.0,
+                presence_penalty = 0.0,
+            )
+            return render(request, 'home.html', {'lang_list':lang_list, 'response':response, 'lang':lang})
+        except Exception as e:
+            print(e)
+            return render(request, 'home.html', {'lang_list':lang_list, 'response':e, 'lang':lang})
 
-    return render(request, 'home.html', {'lang_list': lang_list})
+        return render(request, 'home.html', {'lang_list': lang_list})
